c496b4fe9f556df6db1c92aa7e695cb0c1103f06
dhruv <gohildhruvwork10@gmail.com>
2021-09-15 16:14:03 +0530

    before javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+let queInput = document.querySelector(".que-input").value;
+let aInput = document.querySelector(".a-input").value;
+let bInput = document.querySelector(".b-input").value;
+let cInput = document.querySelector(".c-input").value;
+let dInput = document.querySelector(".d-input").value;
+let submit = document.querySelector(".submit");
+
+submit.addEventListener('click', ()=>{
+   //add all of that to object, send that to db
+
+   // add the question, options, ans to the page
+
+
+   //make it so it won't go away if page relodes
+});
